08e90c47b1ffdf61fc5545329f605c6cf68a2ee1
lavkush gautam <154265093+Lavkush-Gautam@users.noreply.github.com>
2025-03-15 15:00:05 +0530

    Time: 23 ms (20.90%) | Memory: 56 MB (79.10%) - LeetSync

---
+++ 2690-house-robber-iv/house-robber-iv.java
@@ -0,0 +1,29 @@
+class Solution {
+    boolean isPossible(int nums[],int mid,int k){
+        int house=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<=mid){
+                house++;
+                i++;
+            }
+        }
+return house>=k;
+    }
+    public int minCapability(int[] nums, int k) {
+        int n=nums.length;
+        int l=Arrays.stream(nums).min().getAsInt();
+        int r=Arrays.stream(nums).max().getAsInt();
+        int res=r;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(isPossible(nums,mid,k)){
+                res=mid;
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return res;
+    }
+}
