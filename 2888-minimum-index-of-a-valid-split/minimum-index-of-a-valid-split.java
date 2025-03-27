198718d711fbf51d97e90388128b42be5203cda0
lavkush gautam <154265093+Lavkush-Gautam@users.noreply.github.com>
2025-03-27 17:13:21 +0530

    Time: 11 ms (95.51%) | Memory: 56.7 MB (82.05%) - LeetSync

---
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int minimumIndex(List<Integer> nums) {
+        int n=nums.size();
+        int maj=-1;
+        int count=0;
+        for(int num:nums){
+            if(count==0){
+                maj=num;
+                count=1;
+            }
+            else if(num==maj){
+                count++;
+            }
+            else{
+                count--;
+            }
+
+        }
+
+        int majCount=0;
+        for(int num:nums){
+            if(num==maj){
+                majCount++;
+            }
+        }
+
+        count=0;
+        for(int i=0;i<n;i++){
+            if(nums.get(i)==maj){
+                count++;
+            }
+            int remainCount=majCount-count;
+            int n1=i+1;
+            int n2=n-i-1;
+
+            if(count*2>n1 && remainCount*2>n2){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
