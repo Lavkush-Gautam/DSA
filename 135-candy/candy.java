25f62f0575fe0f976320a48491b609cba11b2903
lavkush gautam <154265093+Lavkush-Gautam@users.noreply.github.com>
2025-06-02 21:30:43 +0530

    Time: 3 ms (84.79%) | Memory: 46.4 MB (10.46%) - LeetSync

---
+++ 135-candy/candy.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int candy = n; //Each person given one candy
+
+        int i = 1;
+        while (i < n) {
+
+            if (ratings[i] == ratings[i - 1]) {
+                i++;
+                continue;
+            }
+
+            //For increasing slope
+            int peak = 0;
+            while (i < n && ratings[i] > ratings[i - 1]) {
+                peak++;
+                candy += peak;
+                i++;
+            }
+
+            //For decreasing slope
+            int valley = 0;
+            while (i < n && ratings[i] < ratings[i - 1]) {
+                valley++;
+                candy += valley;
+                i++;
+            }
+
+            
+            candy -= Math.min(peak, valley);
+        }
+
+        return candy;
+    }
+}
